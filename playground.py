88f2dc1b8c66db8150313712a5edd5b082edd8f5
JNF\nicholas <nicfr426@student.liu.se>
2020-11-04 11:13:32 +0100

    newest

--- playground.py
+++ playground.py
@@ -1,1 +1,8 @@
-import TensorFlow as tf
+import tensorflow as tf
+
+def init_inputs(input_size):
+    inputs=tf.placeholder(tf.float32, shape=(None,input_size), name='inputs')
+    print('I used tf.placeholder Nice!')
+    return inputs
+
+    pass
