4178a4e359a3702935f82462e95b984f6fc8eb31
Nichollas <nicfr426@student.liu.se>
2020-11-04 13:24:23 +0100

    Did some more on my main station!

--- playground.py
+++ playground.py
@@ -1,8 +1,12 @@
 import tensorflow as tf
 
 def init_inputs(input_size):
-    inputs=tf.placeholder(tf.float32, shape=(None,input_size), name='inputs')
+    inputs = tf.placeholder(tf.float32, shape=(None, input_size), name='inputs')
     print('I used tf.placeholder Nice!')
     return inputs
 
+def init_labels(output_size):
+    labels = tf.placeholder(tf.int32, shape=(None, output_size), name='labels')
+    return labels
     pass
+
