b18444585fbd13a860a8e332e9860586a7171ca7
Nichollas <nicfr426@student.liu.se>
2020-11-09 09:55:03 +0100

    Did some more on my main station!

--- UsingSqueezeNet.py
+++ UsingSqueezeNet.py
@@ -20,3 +20,80 @@ class SqueezeNetModel(object):
         crop_image = tf.random_crop(float_image, [self.resize_dim, self.resize_dim, 3])
         update_image = tf.image.random_flip_left_right(crop_image)
         return update_image
+
+    # Data Augmentation
+    def image_preprocessing(self, data, is_training):
+        reshaped_image = tf.reshape(data, [3, self.original_dim, self.original_dim])
+        transposed_image = tf.transpose(reshaped_image, [1, 2, 0])
+        float_image = tf.cast(transposed_image, tf.float32)
+        if is_training:
+            updated_image = self.random_crop_and_flip(float_image)
+        else:
+            updated_image = tf.image.resize_image_with_crop_or_pad(float_image, self.resize_dim, self.resize_dim)
+        standardized_image = tf.image.per_image_standardization(updated_image)
+        return standardized_image
+
+        # Convolution layer wrapper
+
+    def custom_conv2d(self, inputs, filters, kernel_size, name):
+        return tf.layers.conv2d(
+            inputs=inputs,
+            filters=filters,
+            kernel_size=kernel_size,
+            activation=tf.nn.relu,
+            padding='same',
+            name=name)
+
+        # SqueezeNet fire module
+
+    def fire_module(self, inputs, squeeze_depth, expand_depth, name):
+        with tf.variable_scope(name):
+            squeezed_inputs = self.custom_conv2d(
+                inputs,
+                squeeze_depth,
+                [1, 1],
+                'squeeze')
+            expand1x1 = self.custom_conv2d(
+                squeezed_inputs,
+                expand_depth,
+                [1, 1],
+                'expand1x1')
+            expand3x3 = self.custom_conv2d(
+                squeezed_inputs,
+                expand_depth,
+                [3, 3],
+                'expand3x3')
+            return tf.concat([expand1x1, expand3x3], axis=-1)
+
+    # Stacked fire modules
+    def multi_fire_module(self, layer, params_list):
+        for param in params_list:
+            layer = self.fire_module(layer, param[0], param[1], param[2])
+        return layer
+
+    # Convolution layer wrapper
+    def custom_conv2d(self, inputs, filters, kernel_size, name):
+        return tf.layers.conv2d(
+            inputs=inputs,
+            filters=filters,
+            kernel_size=kernel_size,
+            activation=tf.nn.relu,
+            padding='same',
+            name=name)
+
+    # Max pooling layer wrapper
+    def custom_max_pooling2d(self, inputs, name):
+        return tf.layers.max_pooling2d(
+            inputs=inputs,
+            pool_size=[2, 2],
+            strides=2,
+            name=name)
+
+    # Model Layers
+    # inputs: [batch_size, resize_dim, resize_dim, 3]
+    def model_layers(self, inputs, is_training):
+        # Initial conv-layer #1
+        conv1 = self.custom_conv2d(inputs, 64, [3, 3], name='conv1')
+        # Initial pool-layer #1
+        pool1 = self.custom_max_pooling2d(conv1, name='pool1')
+        pass
