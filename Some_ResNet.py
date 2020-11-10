424be7638800e7d79c7348d80a05de6d87dc98a0
Nichollas <nicfr426@student.liu.se>
2020-11-10 16:26:00 +0100

    Did some more on my main station!

--- Some_ResNet.py
+++ Some_ResNet.py
@@ -12,8 +12,7 @@ block_layer_sizes = {
 
 class ResNetModel(object):
     # Model Initialization
-    def __init__(self, min_aspect_dim, resize_dim, num_layers, output_size,
-        data_format='channels_last'):
+    def __init__(self, min_aspect_dim, resize_dim, num_layers, output_size, data_format='channels_last'):
         self.min_aspect_dim = min_aspect_dim
         self.resize_dim = resize_dim
         self.filters_initial = 64
@@ -22,6 +21,7 @@ class ResNetModel(object):
         self.output_size = output_size
         self.block_layer_sizes = block_layer_sizes[num_layers]
         self.bottleneck = num_layers >= 50 # True or False
+
 
     # Custom padding function
     def custom_padding(self, inputs, kernel_size):
@@ -47,3 +47,56 @@ class ResNetModel(object):
             inputs=inputs, filters=filters, kernel_size=kernel_size,
             strides=strides, padding=padding, data_format=self.data_format,
             name=name)
+
+        # Apply pre-activation to input data
+
+    def pre_activation(self, inputs, is_training):
+        axis = 1 if self.data_format == 'channels_first' else 3
+        bn_inputs = tf.layers.batch_normalization(inputs, axis=axis, training=is_training)
+        pre_activated_inputs = tf.nn.relu(bn_inputs)
+        return pre_activated_inputs
+
+    # Returns pre-activated inputs and the shortcut
+    def pre_activation_with_shortcut(self, inputs, is_training, shortcut_params):
+        pre_activated_inputs = self.pre_activation(inputs, is_training)
+        shortcut = inputs
+        shortcut_filters = shortcut_params[0]
+        if shortcut_filters is not None:
+            strides = shortcut_params[1]
+            shortcut = self.custom_conv2d(pre_activated_inputs, shortcut_filters, kernel_size=1, strides=strides)
+
+        return pre_activated_inputs, shortcut
+        # ResNet building block
+
+    def regular_block(self, inputs, filters, strides, is_training, index, shortcut_filters=None):
+        with tf.variable_scope('regular_block{}'.format(index)):
+            shortcut_params = (shortcut_filters, strides)
+            pre_activated1, shortcut = self.pre_activation_with_shortcut(inputs, is_training, shortcut_params)
+            conv1 = self.custom_conv2d(pre_activated1, filters, 3, strides)
+            #
+            pre_activated2 = self.pre_activation(conv1, is_training)
+            conv2 = self.custom_conv2d(pre_activated2, filters=filters, kernel_size=3, strides=1)
+            return conv2 + shortcut
+    # ResNet bottleneck block
+    def bottleneck_block(self, inputs, filters, strides, is_training, index, shortcut_filters=None):
+        with tf.variable_scope('bottleneck_block{}'.format(index)):
+            shortcut_params = (shortcut_filters, strides)
+            pre_activated1, shortcut = self.pre_activation_with_shortcut(inputs, is_training, shortcut_params)
+            conv1 = self.custom_conv2d(pre_activated1, filters, 1, 1)
+            pre_activated2 = self.pre_activation(conv1, is_training)
+            conv2 = self.custom_conv2d(pre_activated2, filters, 3, strides)
+            # CODE HERE
+            pre_activated3 = self.pre_activation(conv2, is_training)
+            conv3 = self.custom_conv2d(pre_activated3, filters*4, 1, 1)
+            return conv3 + shortcut
+    # Creates a layer of blocks
+    def block_layer(self, inputs, filters, strides, num_blocks, is_training, index):
+        with tf.variable_scope('block_layer{}'.format(index)):
+            shortcut_filters = 4 * filters if self.bottleneck else filters
+            block_fn = self.bottleneck_block if self.bottleneck else self.regular_block
+            block_output = block_fn(inputs, filters, strides, is_training, 0,
+                                    shortcut_filters=shortcut_filters)
+            # stack the blocks in this layer
+            for i in range(1, num_blocks):
+                block_output = block_fn(block_output, filters, 1, is_training, i)
+            return block_output
